76483f9257c8419174356f42991924ccf645fe5e
b1orhythm <ilya.berdnikov21@gmail.com>
2023-05-18 11:19:42 +0300

    Initial commit

---
+++ test_items.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.mark.parametrize('book', ["the-cathedral-the-bazaar_190", "coders-at-work_207"])
+def test_check_button(browser, book):
+    browser.get(f"http://selenium1py.pythonanywhere.com/catalogue/{book}/")
+    btn_presence = None
+    try:
+        WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, "add_to_basket_form")))
+        btn_presence = True
+    except:
+        btn_presence = False
+
+    assert btn_presence == True, f"Книга '{book}' недоступа для покупки"
